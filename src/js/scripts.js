bc537578614169b02300a725a53ec3ce594da81c
tendo993 <tendo993@gmail.com>
2019-06-24 21:00:20 +0300

    Сверстал и адаптировал карту, футер, страницу с картой

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -129,4 +129,41 @@ $(document).ready(function(e) {
   });
   // Инициализация WOW.js
   new WOW().init();
+  // Карта
+  var client = $('.map');
+  var clientTop = client.offset().top;
+  $(window).bind('scroll', function(){
+    var windowTop = $(this).scrollTop();
+    if (windowTop > clientTop) {
+      $('#map').html('<script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3Ad898f036de8b914820a38c6f07359468827d5cff95ba1fbe277a74f332623d4e&amp;width=100%25&amp;height=666&amp;lang=ru_UA&amp;scroll=false"></script>');
+      console.log('докрутили');
+      $(window).unbind('scroll');
+    }
+  });
+  // Валидация формы
+  $('.phone').mask('+7 (999) 999-99-99');
+  $('#map-form').validate({
+    rules: {
+      user_name: {
+        required: true,
+        minlength: 2,
+        maxlength: 15
+      },
+      user_phone: {
+        required: true,
+        minlength: 10
+      }
+    },
+    messages: {
+      user_name: {
+        required: "Введите Ваше имя",
+        minlength: jQuery.validator.format("Минимум 2 символа"),
+        maxlength: jQuery.validator.format("Максимум 15 символов")
+      },
+      user_phone:{
+        required: "Введите Ваш телефон",
+        minlength: jQuery.validator.format("Минимум 10 символов")
+      }
+    },
+  });
 });
